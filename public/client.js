70431cab1d38d3e6a71c35bbce00396903d5a186
Lazrius <lazdisco@gmail.com>
2025-04-24 14:15:25 +0100

    fix: ensure download page can actually be used

--- public/client.js
+++ public/client.js
@@ -1,32 +1,12 @@
 'use strict';
 
-$(window).on('action:ajaxify.end', function (ev, data) {
-    onPageReady(data);
-});
-
-function onPageReady(data) {
-    const url = data.url;
-
-    const quickReplyBtn = $("button[component='topic/quickreply/button']");
-    if (quickReplyBtn) {
-        handleQuickReply(quickReplyBtn);
-    }
-    else if (url === 'downloads') {
-        renderDownloadArchive();
-    }
-}
-
 function renderDownloadArchive() {
     window.downloading = window.downloading || false;
     const downloadCards = $('a[data-link][data-token][data-name][data-bucket]');
     downloadCards.each(function () {
-        $(this)
-            .on('click', () => window.location.href =
-                `${$(this)
-                    .attr('data-link')}/file/${$(this)
-                    .attr('data-bucket')}/${$(this)
-                    .attr('data-name')}?Authorization=${$(this)
-                    .attr('data-token')}`);
+        const el = $(this);
+        el.on('click', () =>
+            window.location.href = `${el.attr('data-link')}/file/${el.attr('data-bucket')}/${el.attr('data-name')}?Authorization=${el.attr('data-token')}`);
     });
 }
 
@@ -39,3 +19,24 @@ function handleQuickReply(quickReplyBtn) {
         quickReplyBtn.prop("disabled", len < 5);
     });
 }
+
+function onPageReady(data) {
+    const url = data.url;
+
+    const quickReplyBtn = $("button[component='topic/quickreply/button']");
+    if (quickReplyBtn.length) {
+        handleQuickReply(quickReplyBtn);
+    } else if (url === 'downloads') {
+        console.log("Download Page Loaded")
+        renderDownloadArchive();
+    }
+}
+
+(async () => {
+    const hooks = await app.require('hooks');
+
+    hooks.on('action:ajaxify.end', (data) => {
+        console.log(data);
+        onPageReady(data);
+    });
+})();
